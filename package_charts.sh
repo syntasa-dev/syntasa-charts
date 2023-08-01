a0cfbdbd1e3ca15993547441d5cce386f584dd9d
philip.crilley@syntasa.com <philip.crilley@syntasa.com>
2023-08-01 17:56:24 -0400

    Fix package output location

--- package_charts.sh
+++ package_charts.sh
@@ -1,3 +1,3 @@
 helm package charts/syntasa --destination charts/syntasa_package/charts
-helm package charts/syntasa --destination charts
-helm package charts/syntasa_package --destination charts
+helm package charts/syntasa --destination .cr-release-packages
+helm package charts/syntasa_package --destination .cr-release-packages
