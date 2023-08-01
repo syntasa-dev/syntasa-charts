f77bb21f7cc17e20844ef4060e0898f7548e08fd
philip.crilley@syntasa.com <philip.crilley@syntasa.com>
2023-08-01 17:44:23 -0400

    Move chart into subchart

--- package_charts.sh
+++ package_charts.sh
@@ -1,1 +1,3 @@
 helm package charts/syntasa --destination charts/syntasa_package/charts
+# helm package charts/syntasa --destination charts
+# helm package charts/syntasa_package --destination charts
