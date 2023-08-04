5621c8581aa9c08c9bc1a93e1f253681abc444fc
philip.crilley@syntasa.com <philip.crilley@syntasa.com>
2023-08-03 21:31:50 -0400

    Shift notebook SA to subchart

--- package_charts.sh
+++ package_charts.sh
@@ -1,3 +1,2 @@
-helm package charts/syntasa --destination charts/syntasa_package/charts
-helm package charts/syntasa --destination .cr-release-packages
+helm package charts/syntasa_package/charts/syntasa --destination .cr-release-packages
 helm package charts/syntasa_package --destination .cr-release-packages
